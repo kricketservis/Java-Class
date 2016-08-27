ecb6c2b0d86303abaf998a504c21aa0a3c8dde38
kricketservis <kricket.servis@gmail.com>
2016-08-26 20:15:03 -0600

    adding dev server

--- index.php
+++
@@ -1,5 +0,0 @@
-
-<html>
-  <head></head>
-  <body>Test 1 2 3 <br> modifying text </body>
-</html>
---
+++ snippet.js
@@ -0,0 +1,32 @@
+// Javascript Types
+
+undefined
+var undefined = 4;
+null // intentionally undefined
+undefined === null // false
+
+NaN // not a number
+typeof NaN // 'number'
+
+// Never use with, or ==
+
+true, false // boolean
+123 // number
+0x47e9 // number
+
+"apple" // string
+'apple' // string
+
+[1, 2, 3] // array (list)
+[1, "green", false] // still an array
+
+function(){} // functions
+
+// objects
+{
+  name: "value"
+}
+
+"Wed Aug 24 2016 20:38:48 GMT-0600 (MDT)" // Date
+\[a-z,1-9]\/g // Regex
+
